9540dedffd98814b5a0f3a27e54b61df70af38e1
bovink <bovink@sina.com>
2018-07-30 22:39:01 +0800

    model模型

--- json/Model.cpp
+++ json/Model.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "Model.h"
+
+int model::Model::getType() const {
+    return type;
+}
+
+void model::Model::setType(int type) {
+    Model::type = type;
+}
+
+void *model::Model::getModel() const {
+    return model;
+}
+
+void model::Model::setModel(void *model) {
+    Model::model = model;
+}
+
+void model::to_json(json &j, const model::Model &m) {
+
+
+    j = json{
+            {"type", m.getType()}
+    };
+
+    int type = m.getType();
+    if (type == 0) {
+
+        j["model"] = *static_cast<date::Date *>(m.getModel());
+    } else if (type == 1) {
+
+        j["model"] = *static_cast<person::Person *>(m.getModel());
+    }
+}
+
+void model::from_json(const json &j, model::Model &m) {
+
+    m.setType(j.at("type").get<int>());
+    int type = m.getType();
+    if (type == 0) {
+
+        m.setModel(&j.at("model").get<date::Date>());
+    } else if (type == 1) {
+
+        m.setModel(&j.at("model").get<person::Person>());
+    }
+}
--- json/Model.h
+++ json/Model.h
@@ -5,10 +5,38 @@
 #ifndef LEARNCPP_MODEL_H
 #define LEARNCPP_MODEL_H
 
+#include <string>
+#include "json.hpp"
+#include "Date.h"
+#include "Person.h"
 
-class Model {
+using namespace std;
+using json = nlohmann::json;
 
-};
+namespace model {
+
+    class Model {
+
+    public:
+        int getType() const;
+
+        void setType(int type);
+
+        void *getModel() const;
+
+        void setModel(void *model);
+
+
+    private:
+
+        int type;
+        void* model;
+
+    };
+    void to_json(json &j, const Model &m);
+
+    void from_json(const json &j, Model &m);
+}
 
 
 #endif //LEARNCPP_MODEL_H
