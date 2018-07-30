5a87bdfc1c808d5b8173f41d874e47c5b2e00647
bovink <bovink@sina.com>
2018-07-30 22:29:13 +0800

    测试json转换。

--- json/main.cpp
+++ json/main.cpp
@@ -4,12 +4,25 @@
 
 #include <iostream>
 #include "json.hpp"
+#include "Date.h"
+#include "Person.h"
 
 using json = nlohmann::json;
 using namespace std;
 
 int main(){
 
-    cout << "hello" ;
+    json j;
+
+//    j["age"] = 13;
+    j["today"] = "hello";
+
+//    person::Person p = j;
+    date::Date d = j;
+
+//    cout << p.getAge() << endl;
+    cout << d.getToday() << endl;
+
+
     return 0;
 }
