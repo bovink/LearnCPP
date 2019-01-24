fc6787a2f51d0dd17bd9c0cb5e4af8bbb67a9a50
bovink <bovink@sina.com>
2019-01-24 11:28:44 +0800

    使用执行准备，极大减少数据库插入耗时

--- database/main.cpp
+++ database/main.cpp
@@ -12,7 +12,10 @@ using namespace std;
 void createTable();
 
 void insertData1();
+
 void insertData2();
+
+void insertData3();
 
 void queryData();
 
@@ -21,7 +24,7 @@ int main() {
 //    createTable();
 //    queryData();
     auto start = chrono::system_clock::now();
-    insertData1();
+    insertData3();
     auto end = chrono::system_clock::now();
     chrono::duration<double> use = end - start;
     cout << use.count() << endl;
@@ -74,6 +77,40 @@ void insertData2() {
 
     sqlite3_close(pDb);
 
+}
+
+void insertData3() {
+// 插入（替换）
+//    const char *sql_insert = "INSERT INTO t_data (number, name) VALUES (?, ?);";
+//    const char *sql_update = "REPLACE INTO t_data (number, name) VALUES ( ?, ?);";
+//    sqlite3_stmt *stmt;
+//    sqlite3_prepare_v2(_db, sql_update, -1, &stmt, NULL);
+//    sqlite3_bind_text(stmt, 1, "1007", -1, SQLITE_TRANSIENT);
+//    sqlite3_bind_text(stmt, 2, "Dolhin", -1, SQLITE_TRANSIENT);
+//    sqlite3_step(stmt);
+//    sqlite3_finalize(stmt);
+
+    sqlite3 *pDb = nullptr;
+    sqlite3_open("test.db", &pDb);
+    const char *sql = "insert into mytable (name, age, addr) values (?,?,?);";
+
+    sqlite3_exec(pDb, "begin;", nullptr, nullptr, nullptr);
+    sqlite3_stmt *stmt;
+    sqlite3_prepare_v2(pDb, sql, -1, &stmt, nullptr);
+
+    for (int i = 0; i < 1000; ++i) {
+        string name = "lilei" + to_string(i);
+        int age = i;
+        string addr = "addr" + to_string(i);
+
+        sqlite3_reset(stmt);
+        sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_int(stmt, 2, age);
+        sqlite3_bind_text(stmt, 3, addr.c_str(), -1, SQLITE_TRANSIENT);
+    }
+    sqlite3_finalize(stmt);
+
+    sqlite3_exec(pDb, "commit;", nullptr, nullptr, nullptr);
 }
 
 void queryData() {
