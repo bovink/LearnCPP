5c81fd6728cf2a1320ecd677522f3dcf8581cf45
bovink <bovink@sina.com>
2019-01-25 14:00:24 +0800

    string

--- string/StringTest.cpp
+++ string/StringTest.cpp
@@ -2,3 +2,8 @@
 // Created by 吴宗刚 on 2019/1/25.
 //
 
+#include "StringTest.h"
+
+int main() {
+
+}
---
+++ string/StringTest.h
@@ -0,0 +1,14 @@
+//
+// Created by 吴宗刚 on 2019/1/25.
+//
+
+#ifndef LEARNCPP_STRINGTEST_H
+#define LEARNCPP_STRINGTEST_H
+
+
+class StringTest {
+
+};
+
+
+#endif //LEARNCPP_STRINGTEST_H
