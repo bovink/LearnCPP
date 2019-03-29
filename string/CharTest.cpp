ca9295f1bff7fddf064eefe6ecb5c4b8186f3eba
bovink <bovink@sina.com>
2019-03-29 17:33:52 +0800

    创建char测试cpp

--- string/CharTest.cpp
+++ string/CharTest.cpp
@@ -6,8 +6,28 @@
 
 using namespace std;
 
+char a = 'a';
+char b = 'b';
+// 可以改变指针，但不能改变指针指向的值
+// const char*与char const*是一样的
+const char *ptr1 = &a;
+// 可以改变指针指向的值，但不能改变指针
+char *const ptr2 = &a;
+
+void print(char c) {
+    cout << c << endl;
+}
+
 int main() {
-    cout << "go" << endl;
+    print(*ptr1);
+    print(*ptr2);
+
+    ptr1 = &b;
+
+    *ptr2 = b;
+    print(*ptr1);
+    print(*ptr2);
+
     return 0;
 }
 
