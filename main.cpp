a1403c69820f694ea729dc077abe11d9f420e837
bovink <bovink@sina.com>
2019-08-28 17:18:45 +0800

    添加wstring代码

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    locale loc("en_US.UTF-8");
+    wchar_t s = L'，';
+    cout << boolalpha << ispunct(s, loc) << endl;
+    wcout.imbue(locale("zh_cn.utf-8"));
+    wcout << s << endl;
+
+    wstring a = L"你好";
+    wchar_t ss = a[0];
+    cout << boolalpha << ispunct(ss, loc) << endl;
+    wcout << ss << endl;
     return 0;
 }
