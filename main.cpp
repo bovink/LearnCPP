85b49f1335fffc0f9ccffb1a04144d6a7ec822b5
bovink <bovink@sina.com>
2019-09-30 09:18:12 +0800

    添加md5使用代码

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
+
+#include "md5/md5.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string x = "test.jpg";
+
+    string md5string = md5(x);
+    cout << md5string <<endl;
+
+    string md5string2 = md5(x.c_str(), x.length());
+    cout << md5string2 <<endl;
     return 0;
 }
