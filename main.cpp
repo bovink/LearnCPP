764500212a0f68ea8aea586a70e0a437908e8cf5
bovink <bovink@sina.com>
2020-06-11 22:43:53 +0800

    添加gitignore规则

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <memory>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    while(1) {
+
+    }
+    int a =10;
+    std::shared_ptr<int> ptra = std::make_shared<int>(a);
     return 0;
 }
