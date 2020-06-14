1fc18fdbb4ef37d367900af4eaa4147b1fbc41b2
bovink <bovink@sina.com>
2020-06-14 21:02:32 +0800

    添加测试

---
+++ Person.cpp
@@ -0,0 +1,18 @@
+/*
+ * Person.cpp
+ *
+ *  Created on: 2020Äê6ÔÂ14ÈÕ
+ *      Author: Bovink
+ */
+
+#include "Person.h"
+
+Person::Person() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Person::~Person() {
+	// TODO Auto-generated destructor stub
+}
+
