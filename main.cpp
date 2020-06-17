1731821b473dc91595fd0d17b8a2fa9217c682ce
bovink <bovink@sina.com>
2020-06-17 23:41:57 +0800

    删掉除 main.cpp 以外的 C++ 相关代码，让分支作为所有分支的起始分支。

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include "set/tester_map.h"
 
-using namespace std;
 
 int main() {
 
-    auto test = new tester_map();
-    test->addData();
-    test->findData();
-    test->printAllKey();
     return 0;
 }
