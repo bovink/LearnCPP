7b726113c23c0191ae5d2ec35304d8be10acc0ec
bovink <bovink@sina.com>
2020-07-11 23:09:00 +0800

    添加声明测试类

---
+++ DeclareTest.cpp
@@ -0,0 +1,20 @@
+/*
+ * DeclareTest.cpp
+ *
+ *  Created on: 2020年7月11日
+ *      Author: Bovink
+ */
+
+#include "DeclareTest.h"
+
+int DeclareTest::x = 0;
+DeclareTest::DeclareTest() {
+	// TODO Auto-generated constructor stub
+	x = 1;
+
+}
+
+DeclareTest::~DeclareTest() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ DeclareTest.h
@@ -0,0 +1,20 @@
+/*
+ * DeclareTest.h
+ *
+ *  Created on: 2020年7月11日
+ *      Author: Bovink
+ */
+
+#ifndef DECLARETEST_H_
+#define DECLARETEST_H_
+
+class DeclareTest {
+public:
+	DeclareTest();
+	virtual ~DeclareTest();
+private:
+
+	static int x;
+};
+
+#endif /* DECLARETEST_H_ */
