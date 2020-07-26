b3e96f395d172dbce5b6a8b8817489b0b6651205
bovink <bovink@sina.com>
2020-07-26 21:08:48 +0800

    添加auto_ptr测试

--- test_auto_ptr/main.cpp
+++ test_auto_ptr/main.cpp
@@ -19,36 +19,79 @@ void testNormalPtr() {
 void testAutoPtr() {
 	/*
 	 * auto_ptr 在 C++ 11 中已经被淘汰。
-	 * 智能指针可以通过 -> 直接访问指针成员。
+	 * 通过  -> 可以像类的指针一样操作类的 public 成员。
+	 * 通过 . 使用 auto_ptr 的方法。
 	 *
 	 * 成员方法：
 	 * get() 返回指针成员
 	 * reset(新指针) 释放原来持有的指针成员，将新指针作为新的指针成员，新指针参数可以为空。
-	 *
+	 * release() 这个函数只是把智能指针赋值为空，但是它原来指向的内存并没有被释放，相当于它只是释放了对资源的所有权。
 	 */
 
-	auto_ptr<Test> _t(new Test("123"));
-	_t->setS("hello");
-	_t->print();
-	_t.get()->print();
+	/*
+	 * 测试操作符
+	 */
+	cout << "==========操作符测试==========" << endl;
+	auto t1 = new Test("操作符测试");
+	auto_ptr<Test> _t1(t1);
+	_t1->setS("hello");
+	_t1.get()->print();
+	(*_t1).setS("world");
+	_t1.get()->print();
 
-	(*_t).setS("world");
-	_t->print();
-	_t.reset(new Test("456"));
-	_t->print();
+	/*
+	 * get() 方法测试
+	 * 通过 get() 可以获取类的指针
+	 */
+	cout << "==========get() 方法测试==========" << endl;
+	auto t2 = new Test("get() 测试");
+	auto_ptr<Test> _t2(t2);
+	_t2.get()->print();
 
-	cout << "\n";
-	auto_ptr<Test> _t2(new Test("789"));
-	_t = _t2;
-	_t->print();
-	if (_t2.get() == NULL)
-		cout << "t2 is NULL" << endl;
-
-	auto t = new Test("aaa");
-	auto_ptr<Test> _t3(t);
+	/*
+	 * release() 方法测试
+	 */
+	cout << "==========release() 方法测试==========" << endl;
+	auto t3 = new Test("release fun test");
+	auto_ptr<Test> _t3(t3);
 	_t3.release();
-	t->print();
-	delete t;
+
+	/*
+	 * reset() 方法测试
+	 */
+	cout << "==========reset() 方法释放指针测试==========" << endl;
+	auto t4 = new Test("reset() 释放智能指针中的指针测试 ");
+	auto_ptr<Test> _t4(t4);
+	_t4.reset();
+
+	/*
+	 * 判断智能指针是否为 NULL
+	 */
+	cout << "==========判断 auto_ptr 是否为空==========" << endl;
+	auto t5 = new Test("判断auto_ptr是否为空");
+	auto_ptr<Test> _t5(t5);
+	_t5.reset();
+	if (_t5.get() == NULL) {
+		cout << "t5 为 NULL" << endl;
+	} else {
+		cout << "t5 不为 NULL" << endl;
+	}
+
+	/*
+	 * 测试赋值操作
+	 */
+	cout << "==========测试赋值操作==========" << endl;
+	auto t6 = new Test("赋值操作123");
+	auto t7 = new Test("赋值操作456");
+	auto_ptr<Test> _t6(t6);
+	auto_ptr<Test> _t7(t7);
+	_t6 = _t7;
+	_t6->print();
+	if (_t7.get() == NULL) {
+		cout << "t7已经释放" << endl;
+	}
+
+	cout << "==========测试结束==========" << endl;
 }
 
 using shareT= shared_ptr<Test>;
@@ -75,20 +118,13 @@ void testSharedPtrMap() {
 	cout << m["1"].use_count() << endl;
 //	cout << _m1.use_count() << endl;
 
-
-	for(auto n :m){
-		n.second->print();
-	}
-//	while(1){
-//
+//	for(auto n :m){
+//		n.second->print();
 //	}
-//	m["1"].reset();
-//	m["2"]->print();
-//	m["3"]->print();
 }
 
-//int main() {
-//	testSharedPtrMap();
-//
-//	return 0;
-//}
+int main() {
+	testAutoPtr();
+
+	return 0;
+}
--- test_ptr_vector/mian.cpp
+++ test_ptr_vector/mian.cpp
@@ -5,11 +5,11 @@
  *      Author: bovink
  */
 
-
 #include <vector>
 #include <iostream>
 #include <algorithm>
 #include "../test_auto_ptr/Test.h"
+#include <memory>
 
 using namespace std;
 
@@ -21,11 +21,11 @@ void testVector() {
 	v.push_back("world");
 	v.push_back("world");
 
-	auto i = find(v.begin(),v.end(),"world");
+	auto i = find(v.begin(), v.end(), "world");
 
 	v.erase(i);
-	v.insert(v.end(),"hello");
-	for(auto s :v){
+	v.insert(v.end(), "hello");
+	for (auto s : v) {
 		cout << s << endl;
 	}
 
@@ -42,10 +42,9 @@ void testPtrPass() {
 	_t1->printPtrs();
 
 }
-int main() {
-
-testPtrPass();
-return 0;
-}
-
-
+//int main() {
+//
+//	testPtrPass();
+//	return 0;
+//}
+//
