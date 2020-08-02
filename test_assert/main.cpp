7e811f46735c2f9fd2a1a73b0005533753f29552
bovink <bovink@sina.com>
2020-08-02 23:29:28 +0800

    添加try catch测试

--- test_assert/main.cpp
+++ test_assert/main.cpp
@@ -8,6 +8,7 @@
 #include "../stdcommon.h"
 //#define NDEBUG
 #include <cassert>
+#include <exception>
 
 void print(const string *s) {
 
@@ -17,7 +18,20 @@ void print(const string *s) {
 }
 
 int main() {
-	string s = "";
-	print(&s);
+//	string s = "";
+//	print(&s);
+	vector<int> a(2);
+	string* b = NULL;
+	try{
+//		if(b == NULL) __throw_out_of_range("the point is NULL");
+//		cout << *b << endl;
+
+		int b = a.at(2);
+		cout << "测试"<<b << endl;
+
+	}catch(...){
+		cerr << "unkown exception" << endl;
+		return -2;
+	}
 	return 0;
 }
