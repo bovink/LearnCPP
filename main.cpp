af8174636982498ce30b6b94b3286be6006668a5
bovink <bovink@sina.com>
2020-11-12 23:40:09 +0800

    添加string用法

--- main.cpp
+++ main.cpp
@@ -6,9 +6,36 @@ void print(const int *v) {
 
 	cout << *v << endl;
 }
+void testConstructor(const string&s,const int&end ){
+
+	auto str = string(s.begin(),s.begin()+end);
+
+	cout << str <<endl;
+
+}
+
+void testFind(const string& s) {
+
+	auto it = s.find('?');
+
+	auto str = string(s.begin(),s.begin()+it);
+
+	cout << str <<endl;
+}
+
+void testrFind(const string& s) {
+
+	auto it = s.rfind('?');
+
+	auto str = string(s.begin()+it,s.end());
+
+	cout << str <<endl;
+}
+
 void testStringFind() {
 	string s = "aa/bb/cc";
-	int i = s.find("/");
+	int i = s.find("/",3);
+
 
 	print(&i);
 	i = s.rfind("/");
@@ -40,7 +67,26 @@ void testStringFind() {
 }
 
 int main() {
+	testFind("11?22");
+	testrFind("11?22");
+	testConstructor("11?22",3);
 
 	testStringFind();
+	string s = string(3,'1');
+	cout <<s<<endl;;
+	const char *cc = "aaaaaaaaaaaaaal";
+	auto s2 = string(cc);
+	cout << s2 << endl;
+	cout << s2.max_size() << endl;
+	cout << s2.size() << endl;
+	cout << s2.length() << endl;
+	s2.resize(25,'1');
+	cout << s2 << endl;
+	cout << s2.data() << endl;
+	cout << s2.c_str() << endl;
+	cout << s2[26] << endl;
+//	cout << s2.at(26) << endl;
+
+
 	return 0;
 }
