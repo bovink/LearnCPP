38d8d4ef376d09d2f316cdb232bfafdd08fb8a43
bovink <bovink@sina.com>
2020-11-29 14:27:21 +0800

    测试第17章17.9.3后的相关代码

--- main.cpp
+++ main.cpp
@@ -1,53 +1,107 @@
 #include <iostream>
-#include <memory>
 #include <vector>
-#include <thread>
-#include <fstream>
-#include "Person.h"
+#include <string>
 
 using namespace std;
 
-void print(vector<double> v) {
+struct Link {
+    string value;
+    Link *prev;
+    Link *succ;
 
-	cout << "{";
-	for (unsigned int i=0;i<v.size();i++ ){
-		cout << v[i];
+    Link(const string &v, Link *p = nullptr, Link *s = nullptr) : value(v), prev(p), succ(s) {}
+};
 
-		v[i] = 1;
-		cout <<",";
-	}
-	cout << "}\n";
+Link *insert(Link *p, Link *n) {// insert n before p,return n
+    if (n == nullptr)return p;
+    if (p == nullptr)return n;
+    n->succ = p;
+    if (p->prev) p->prev->succ = n;
+    n->prev = p->prev;
+    p->prev = n;
+    return n;
 }
-void test() {
-	cout << "go" << endl;
+
+Link *erase(Link *p) {
+    if (p == 0) return 0;
+    if (p->succ) p->succ->prev = p->prev;
+    if (p->prev) p->prev->succ = p->succ;
+    return p->succ;
+}
+
+Link *find(Link *p, const string &s) {
+    while (p != nullptr) {
+        if (p->value == s)return p;
+        p = p->succ;
+    }
+    return nullptr;
+}
+
+void print_all(Link *p) {
+    cout << "{";
+    while (p) {
+        cout << p->value;
+        if ((p = p->succ)) cout << ",";
+
+    }
+    cout << "}";
+
+}
+
+void print_all_reverse(Link *p) {
+    while (p->succ) {
+        p = p->succ;
+    }
+    cout << "{";
+    while (p) {
+        cout << p->value;
+        if ((p = p->prev)) cout << ",";
+
+    }
+    cout << "}";
+
+}
+
+void test (){
+
+
+    Link *norse_gods = new Link("Thor");
+    norse_gods = insert(norse_gods, new Link("Odin"));
+    norse_gods = insert(norse_gods, new Link("Zeus"));
+    norse_gods = insert(norse_gods, new Link("Freia"));
+
+    Link *greek_gods = new Link("Hera");
+    greek_gods = insert(greek_gods, new Link("Athena"));
+    greek_gods = insert(greek_gods, new Link("Mars"));
+    greek_gods = insert(greek_gods, new Link("Poseidon"));
+
+    Link *p = find(norse_gods, "Freia");
+    if (p) {
+        if (p == norse_gods) norse_gods = p->succ;
+        erase(p);
+        greek_gods = insert(greek_gods, p);
+    }
+    print_all_reverse(norse_gods);
+    cout << "\n";
+
+    print_all(norse_gods);
+    cout << "\n";
+
+    print_all(greek_gods);
+    cout << "\n";
+
 }
 int main() {
-	vector<double> vd(10);
-	print(vd);
-	print(vd);
+    try {
 
-	int a = 10;
-	int* pint = &a;
-	cout << *(pint+1) << endl;
-	cout << pint[1] << endl;
+        vector<string> strings(5, "1");
+        string s = strings.at(2);
+        cout << s << endl;
+    } catch (exception &e) {
 
-	int& r = a;
-	cout << r << endl;
-	cout << r << endl;
-	string name = "CMakeLists.txt";
-	ifstream ifs;
-	ifs.open(name,ifstream::in);
-//	char c = ifs.get();
-	char c = ifs.get();
-	while (ifs.good()){
-	    
-
-//		cout << ifs;
-		cout << c;
-		c = ifs.get();
-	}
-	ifs.close();
-	Person::Test* test = new Person::Test();
-	return 0;
+        cerr << e.what();
+        return 1;
+    }
+    return 0;
 }
 
