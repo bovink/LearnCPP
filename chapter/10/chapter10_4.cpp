95d4961d4abd01cc70709637a5fe71945680648e
bovink <bovink@sina.com>
2020-11-30 16:50:48 +0800

    添加10_4和10_5章节实践

--- chapter/10/chapter10_4.cpp
+++ chapter/10/chapter10_4.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "chapter10_4.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+void writeFile() {
+    string name = "test.txt";
+    string path = "/Users/bovink/Development/MyProject/CppProject/LearnCPP/";
+    ofstream ofs(path + name);
+    if (!ofs) cerr << "Error! File Not Exist";
+    ofs << "Hello World!";
+}
+
+void readFile() {
+    string name = "test.txt";
+    string path = "/Users/bovink/Development/MyProject/CppProject/LearnCPP/";
+    ifstream ifs(path + name);
+    if (!ifs) cerr << "Error! File Not Exist";
+    string s;
+    while (ifs >> s) {
+        cout << s << endl;
+    }
+}
+
+//int main() {
+//    writeFile();
+//    readFile();
+//    return 0;
+//}
+//
--- chapter/10/chapter10_5.cpp
+++ chapter/10/chapter10_5.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "chapter10_5.h"
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string PATH = "/Users/bovink/Development/MyProject/CppProject/LearnCPP/";
+
+struct Reading {
+    int hour;
+    double temperature;
+
+    Reading(int hour, double temperature) : hour(hour), temperature(temperature) {}
+};
+
+int main() {
+    cout << "Please enter input file name:";
+    string name;
+    cin >> name;
+    ifstream ifs(PATH + name);
+    if (!ifs) cerr << "File Not Exist: " << name;
+    cout << "Please enter name of output file:";
+    cin >> name;
+    ofstream ofs(PATH + name);
+    if (!ofs) cerr << "Can't open File " << name;
+
+    vector<Reading> temps;
+    int hour;
+    float temperature;
+    while (ifs >> hour >> temperature) {
+        if (hour < 0 || hour > 23) cerr << "hour out of range";
+        temps.push_back(Reading(hour, temperature));
+    }
+
+    for (int i = 0; i < temps.size(); ++i) {
+        ofs << '(' << temps.at(i).hour << ','
+            << temps.at(i).temperature << ")\n";
+    }
+
+}
+
