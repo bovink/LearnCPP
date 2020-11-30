4927751cded65def0b55ed150e0533341c8b73ce
bovink <bovink@sina.com>
2020-11-30 15:48:23 +0800

    还原main

--- main.cpp
+++ main.cpp
@@ -1,53 +1,5 @@
-#include <iostream>
-#include <memory>
-#include <vector>
-#include <thread>
-#include <fstream>
-#include "Person.h"
 
-using namespace std;
-
-void print(vector<double> v) {
-
-	cout << "{";
-	for (unsigned int i=0;i<v.size();i++ ){
-		cout << v[i];
-
-		v[i] = 1;
-		cout <<",";
-	}
-	cout << "}\n";
-}
-void test() {
-	cout << "go" << endl;
-}
 int main() {
-	vector<double> vd(10);
-	print(vd);
-	print(vd);
-
-	int a = 10;
-	int* pint = &a;
-	cout << *(pint+1) << endl;
-	cout << pint[1] << endl;
-
-	int& r = a;
-	cout << r << endl;
-	cout << r << endl;
-	string name = "CMakeLists.txt";
-	ifstream ifs;
-	ifs.open(name,ifstream::in);
-//	char c = ifs.get();
-	char c = ifs.get();
-	while (ifs.good()){
-	    
-
-//		cout << ifs;
-		cout << c;
-		c = ifs.get();
-	}
-	ifs.close();
-	Person::Test* test = new Person::Test();
 	return 0;
 }
 
