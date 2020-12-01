0beebbd95583d37f3ecd0be372a2406dc69a105c
bovink <bovink@sina.com>
2020-12-01 22:25:51 +0800

    添加10_10实现

--- chapter/10/chapter10_10.cpp
+++ chapter/10/chapter10_10.cpp
@@ -2,3 +2,38 @@
 // Created by 吴宗刚 on 2020-12-01.
 //
 
+#include "../../std_lib_facilities.h"
+
+const int not_a_reading = -7777;
+const int not_a_month = -1;
+
+struct Day {
+    vector<double> hour;
+
+    Day();
+};
+
+Day::Day() : hour(24) {
+    for (int i = 0; i < hour.size(); ++i) {
+        hour[i] = not_a_reading;
+    }
+}
+
+struct Month {
+    int month;
+    vector<Day> day;
+
+    Month() : month(not_a_month), day(32) {}
+};
+
+struct Year {
+    int year;// positive == A.D.
+    vector<Month> month;
+
+    Year() : month(12) {}
+};
+
+
+
+
+
