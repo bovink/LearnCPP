975800e5a5c949c0c226d03ec369c943e103c34e
bovink <bovink@sina.com>
2020-12-01 14:03:01 +0800

    添加公共头文件依赖

---
+++ std_lib_facilities.h
@@ -0,0 +1,17 @@
+//
+// Created by 吴宗刚 on 2020-12-01.
+//
+
+#ifndef LEARNCPP_STD_LIB_FACILITIES_H
+#define LEARNCPP_STD_LIB_FACILITIES_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <memory>
+
+using namespace std;
+
+#endif //LEARNCPP_STD_LIB_FACILITIES_H
+
