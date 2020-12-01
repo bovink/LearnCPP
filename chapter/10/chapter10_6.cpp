7389ba2369b0d474d5bcff2ced3b0fe8596d7920
bovink <bovink@sina.com>
2020-12-01 14:03:12 +0800

    注释main函数 添加I/O错误处理

--- chapter/10/chapter10_6.cpp
+++ chapter/10/chapter10_6.cpp
@@ -2,3 +2,29 @@
 // Created by 吴宗刚 on 2020-12-01.
 //
 
+#include "../../std_lib_facilities.h"
+
+void fill_vector(istream &ist, vector<int> &v, char terminator)
+// read integers from ist into v until we reach eof() or terminator
+{
+    int i = 0;
+    while (ist >> i) v.push_back(i);
+    if (ist.eof()) return;
+    // not good() and not bad() and not eof(), ist must be fail()
+    ist.clear();
+
+    char c;
+    ist >> c;
+    if (c != terminator) {
+        ist.unget();
+        ist.clear(ios_base::failbit);
+    }
+
+}
+
+
+//int main() {
+//
+//    cout << "chapter10_6";
+//    return 0;
+//}
