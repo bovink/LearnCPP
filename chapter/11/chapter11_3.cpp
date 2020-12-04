a864083e36abba358c1f2c5db733f18d41b1f7ef
bovink <bovink@sina.com>
2020-12-04 22:18:57 +0800

    添加2进制文件读写

--- chapter/11/chapter11_3.cpp
+++ chapter/11/chapter11_3.cpp
@@ -6,7 +6,7 @@
 void input_app() {
 
     string path = "/Users/bovink/Development/MyProject/CppProject/LearnCPP/";
-    ofstream ofs(path+"somefile.txt",ios_base::app);
+    ofstream ofs(path + "somefile.txt", ios_base::app);
     ofs << " hello";
 
 }
@@ -16,6 +16,40 @@ void file_not_exist() {
     string path = "/Users/bovink/Development/MyProject/CppProject/LearnCPP/";
     ofstream ofs(path + "no_such_file.txt");// write mode, will create a file
     ifstream ifs(path + "no_file.txt");// read mode,file not exist will cause error
+}
+
+template<class T>
+char *as_bytes(T &i) {
+    void *addr = &i;
+    return static_cast<char *> (addr);
+}
+
+void test_binary() {
+    cout << "Please enter inpout file name\n";
+    string name;
+    cin >> name;
+    ifstream ifs(name.c_str(), ios_base::binary);
+    if (!ifs) {
+        cerr << "can't open input file ", name);
+        return;
+    }
+    cout << "Please enter output file name\n";
+    cin >> name;
+    ofstream ofs(name.c_str(), ios_base::binary);
+    if (!ofs) {
+        cerr << "can't open output file ", name);
+        return;
+    }
+
+    vector<int> v;
+
+    int i;
+    while (ifs.read(as_bytes(i), sizeof(int)))
+        v.push_back(i);
+
+    for (int j = 0; j < v.size(); ++j) {
+        ofs.write(as_bytes(v[i]), sizeof(int));
+    }
 }
 
 void test11_3() {
