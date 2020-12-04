3a1fb2d6f3850a191649cf8e86ab7a4d2af2ea9f
bovink <bovink@sina.com>
2020-12-04 10:46:03 +0800

    添加文件不存在时读写操作的差异测试 添加文件的追加写模式测试

--- chapter/11/chapter11_3.cpp
+++ chapter/11/chapter11_3.cpp
@@ -1,4 +1,29 @@
 //
 // Created by Bovink on 2020/12/2.
 //
+#include "std_lib_facilities.h"
 
+void input_app() {
+
+    string path = "/Users/bovink/Development/MyProject/CppProject/LearnCPP/";
+    ofstream ofs(path+"somefile.txt",ios_base::app);
+    ofs << " hello";
+
+}
+
+void file_not_exist() {
+
+    string path = "/Users/bovink/Development/MyProject/CppProject/LearnCPP/";
+    ofstream ofs(path + "no_such_file.txt");// write mode, will create a file
+    ifstream ifs(path + "no_file.txt");// read mode,file not exist will cause error
+}
+
+void test11_3() {
+    input_app();
+//    file_not_exist();
+}
+
+int main() {
+    test11_3();
+    return 0;
+}
